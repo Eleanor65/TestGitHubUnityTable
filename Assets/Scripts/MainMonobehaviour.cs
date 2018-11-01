488dcf03e4bcc3b85f0372427ccdb231d422ae9b
VsevolodPiletski <sevadantes@gmail.com>
2018-11-01 09:10:18 +0300

    Created MainScript, renamed scene. Test of WWW

---
+++ Assets/Scripts/MainMonobehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GitHubUnityTable
+{
+    public class MainMonobehaviour : MonoBehaviour
+    {
+        private const string Url = "https://api.github.com/users/Eleanor65/repos";
+
+        private IEnumerator Start()
+        {
+            using (WWW www = new WWW(Url))
+            {
+                yield return www;
+                Debug.Log(www.text);
+            }
+        }
+    }
+}
