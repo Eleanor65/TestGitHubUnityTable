7d3c2db7ee5e55075065603734ffeebb3aa979b8
VsevolodPiletski <sevadantes@gmail.com>
2018-11-05 12:25:38 +0300

    Tested parsing jasonArray

--- Assets/Scripts/MainMonobehaviour.cs
+++ Assets/Scripts/MainMonobehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Newtonsoft.Json.Linq;
 using UnityEngine;
 
 namespace GitHubUnityTable
@@ -7,13 +8,23 @@ namespace GitHubUnityTable
     {
         private const string Url = "https://api.github.com/users/Eleanor65/repos";
 
-        private IEnumerator Start()
+        private string _repoInfoString;
+        
+        private void Start()
+        {
+            StartCoroutine(DownloadRepoInfoString());
+        }
+
+        private IEnumerator DownloadRepoInfoString()
         {
             using (WWW www = new WWW(Url))
             {
                 yield return www;
-                Debug.Log(www.text);
+                _repoInfoString = www.text;
             }
+
+            var jArray = JArray.Parse(_repoInfoString);
+            Debug.Log(jArray);
         }
     }
 }
