8ddc9880b2bba292fc424f5875803eb8323db87a
VsevolodPiletski <sevadantes@gmail.com>
2018-11-05 16:01:13 +0300

    Tool for clearing player prefs

---
+++ Assets/Scripts/Editor/Tools.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace GitHubUnityTable
+{
+    public static class Tools
+    {
+        [MenuItem("Tools/Clear Player Prefs", false, 1)]
+        public static void ClearPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
